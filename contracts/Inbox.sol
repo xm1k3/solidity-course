1e4acf66c25f8dcc698ccfd2bd530c0340c10b94
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2024-03-15 00:16:50 +0100

    feat: updated smart contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
-
+ 
+pragma solidity ^0.8.9;
+ 
 contract Inbox {
     string public message;
-
-    function Inbox(string initialMessage) public {
+    
+    constructor(string memory initialMessage) {
         message = initialMessage;
     }
-
-    function setMessage(string newMessage) public {
+    
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
 }
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,47 +2,35 @@ const ganache = require("ganache")
 const { Web3 } = require("web3")
 const assert = require("assert")
 const web3 = new Web3(ganache.provider())
-const { interface, bytecode } = require("../compile")
+const { abi, evm } = require("../compile")
 
 let accounts
 let inbox
 const INITIAL_STRING = "sassarello"
 
+ 
 beforeEach(async () => {
-    // Get a list of all accounts
-
-    accounts = await web3.eth.getAccounts()
-
-    // use una of this accounts to deploy
-    // the contract
-
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode,
-            arguments: [INITIAL_STRING]
-        })
-        .send({
-            from: accounts[0],
-            gas: "1000000"
-        })
-
-})
-
-describe("Inbox", () => {
-    it("deploy a contract", () => {
-        assert.ok(inbox.options.address)
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [INITIAL_STRING],
     })
-
-    it("has a default message", async () => {
-        const message = await inbox.methods.message().call()
-        assert.equal(message, INITIAL_STRING)
-    })
-
-    it("can change the message", async () => {
-        txHash = await inbox.methods.setMessage("sassarello 2").send({
-            from: accounts[0]
-        })
-        const message = await inbox.methods.message().call()
-        assert.equal(message, "sassarello 2")
-    })
-})
+    .send({ from: accounts[0], gas: '1000000' });
+});
+ 
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+});
