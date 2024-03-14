4e99a03adc6c786e9b7a34f2e2a980d1bc1e7f62
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2024-03-14 23:59:19 +0100

    feat: added the deploy file

--- deploy.js
+++ deploy.js
@@ -1,5 +1,31 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require("./compile")
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+  "MNEMONIC HERE",
+  "INFURA URL HERE"
+)
+
+const web3 = new Web3(provider )
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    console.log("account:", accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ["sassarello"]
+        })
+        .send({
+            gas: "1000000", 
+            from: accounts[0]
+        })
+
+    console.log(result.options.address)
+    provider.engine.stop()
+}
+
+deploy()
